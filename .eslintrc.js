f5f501c6a00d291eb2866f2f50946ddfd39397d3
darmikon <hellfrom@mail.ru>
2018-03-13 00:25:03 +0200

    initial

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    "extends": "airbnb",
+    "rules": {
+        "func-names": ["error", "never"]
+    },
+    "env": {
+        "mocha": true,
+        "node": true,
+    }
+};
